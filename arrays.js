1ec660a4d77619cab1e89a548914fcbe97b95b43
climoralesg <climoralesg@gmail.com>
2020-01-13 06:20:53 -0400

    avance de tutorial

--- arrays.js
+++ arrays.js
@@ -35,3 +35,89 @@ for(let i=0;i<arreglo2.length;i++)
 console.log(arreglo2);
 
 //PROPIEDADES
+//.length se puede saber cuantos elementos tiene un array 
+var arreglo3= [1, "plátano", "manzana"];
+console.log(arreglo3.length);
+
+var numeros = [1, 2, 3, 4, 5];
+for (var i = 0; i < numeros.length; i++) {
+    console.log(numeros[i] *= 10);
+}
+
+//METODOS
+
+//.isArray() retorna un boleano en funcion desi el parametro es un array o no
+
+var arreglo4=[1,2,3];
+//true
+console.log(Array.isArray([1]));
+console.log(Array.isArray(arreglo4));
+
+//false
+console.log(Array.isArray());
+console.log(Array.isArray({}));
+console.log(Array.isArray(null));
+console.log(Array.isArray(undefined));
+
+//.sort() permite organizar los elementos de un array, por orden alfabetico o en funcion numerica (ascendente)
+
+var frutas = ['plátano', 'Naranja', 'Limón', 'Manzana', 'Mango'];
+frutas.sort(); // ["Limón", "Mango", "Manzana", "Naranja", "plátano"]
+var numeros = [0, 45, 2, -5, 123, -47];
+numeros.sort() // [-47, -5, 0, 123, 2, 45]
+
+//algunas veces el ordenado no puede funcionar como esperamos, si los elementos que componen el array son de una naturaleza
+//distinta
+
+var miArray = ['uno', 2, true, 'más datos...'];
+miArray.sort(); // [2, "más datos...", true, "uno"]
+
+//.reverse() invierte el orden de un array
+var miArray2=['uno',2,true,'mas datos...'];
+console.log(miArray2);
+miArray2.reverse();
+console.log(miArray2);
+
+//.join() retorna una cadena con todos los elementos de array dentro (reemplaza)
+var array = ['dato1', 2, 'masDatos'];
+var datosJuntos = array.join(); // 'dato1,2,masDatos'
+var datosJuntos2 = array.join(''); // 'dato12masDatos'
+var datosJuntos3 = array.join(' + '); // 'dato1 + 2 + masDatos'
+console.log(datosJuntos);
+console.log(datosJuntos2);
+console.log(datosJuntos3);
+
+//.toString() retorna una cadena de texto con todos los elementos
+
+var amigos=['Luis', 'Carlos', 'Marco', 'Eduardo'];
+console.log(amigos.toString());
+
+//.toLocateString() retorna como string (configuracion regional) todos los elementos
+
+var numero=1337.89;
+var fecha=new Date();
+var miArray3=[numero,fecha,'mas datos'];
+
+var arrayConvertida=miArray.toLocaleString();
+console.log(arrayConvertida);
+
+//.concat() retorna un nuevo array con los arrays especificados concatenados
+//dos arrays:
+
+var arreglo5=['a',2,true];
+    var arreglo6=[1,2,4];
+    var nuevaArray=arreglo5.concat(arreglo6);
+console.log(nuevaArray);
+
+// multiples arrays
+var arreglo7 = ['a', 2, true];
+    var arreglo8 = [1, 2, 4];
+    var otroArreglo = ['abc', 1, false] 
+    var nuevaArray2 = arreglo7.concat(arreglo8, [5.25, 100], otroArreglo);    
+console.log(nuevaArray2);
+
+//.indexOf() devuelve la posicion donde encuentra el elemento en si o -1, si no lo encuentra
+
+var array2=[2,5,9];
+console.log(array2.indexOf(9)); //2
+console.log(array2.indexOf(12)); // -1
--- dates.js
+++ dates.js
@@ -33,3 +33,67 @@ var newYearCuatro=new Date(Date.UTC(2016,1,1));
 console.log(newYearCuatro);
 
 //METODOS
+/*Todos aquellos métodos que son susceptibles de sufrir variaciones por la zona
+horaria, cuentan con un “método clon” que realiza la misma función, pero siguiendo
+las especificaciones de la UTC, como veremos a continuación.
+
+Por otro lado, los meses y los días de la semana, empiezan a contarse desde el
+0, y los días de la semana empiezan en domingo siendo este el día 0. El resto de
+componentes no sufren modificaciones (año, día del mes, etc..). */
+
+
+//GETER
+// Versión UTC:
+var ahora = new Date();
+console.log("Con UTC: ");
+console.log("==== FECHA ====");
+console.log("El año: " + ahora.getUTCFullYear()); // 4 dígitos
+console.log("El mes: " + ahora.getUTCMonth()); // 0 - 11
+console.log("El día de la semana: " + ahora.getUTCDay()); // 0 - 6 (D - S)
+console.log("El día del mes: " + ahora.getUTCDate()); // 1-31
+console.log("==== HORA ====");
+console.log("Horas: " + ahora.getUTCHours());
+ console.log("Minutos: " + ahora.getUTCMinutes());
+ console.log("Segundos: " + ahora.getUTCSeconds());
+ console.log("milisegundos: " + ahora.getUTCMilliseconds());
+
+ //Versión Local:
+var ahora = new Date();
+console.log("La fecha es " + ahora);
+console.log("==== FECHA ====");
+console.log("El año: " + ahora.getFullYear()); // 4 dígitos
+console.log("El mes: " + ahora.getMonth()); // 0 - 11
+console.log("El día de la semana: " + ahora.getDay()); // 0 - 6 (D - S)
+console.log("El día del mes: " + ahora.getDate()); // 1-31
+console.log("==== HORA ====");
+console.log("Horas: " + ahora.getHours());
+ console.log("Minutos: " + ahora.getMinutes());
+ console.log("Segundos: " + ahora.getSeconds());
+ console.log("Milisegundos desde 1/1/1970: "+ ahora.getTime());
+ console.log("milisegundos: " + ahora.getMilliseconds());
+ console.log("==== OTROS ====");
+ console.log("Diferencia horaria respecto a UTC: " + ahora.getTimezoneOffset());
+
+ //Setters
+ /*Los setters en el caso de las fechas están planteados como una manera de ajustar la fecha y
+la hora dentro de un marco determinado. Veamos en el siguiente ejemplo:*/
+
+//Dentro del marco
+var newYear = new Date(Date.UTC(2016, 1, 1));
+console.info("La fecha es " + newYear);
+newYear.setFullYear(1980); // Pasamos a 1980
+console.info("La fecha es " + newYear);
+newYear.setUTCMilliseconds(1500); // 1500ms en formato UTC
+console.info("La fecha es " + newYear);
+
+
+//Fuera del marco
+/*Debes tener en cuenta las leyes naturales: un día tiene 24 horas, un mes tiene un
+máximo de 31 días…
+Si obviamos esta lógica, JavaScript improvisa una nueva fecha, pero ésta no suele ser
+correcta.*/
+newYear.setDate(56); // Al día 56 de Enero
+console.info("La fecha es " + newYear);
+newYear.setUTCHours(36); // Pasamos a la hora 36 del día
+console.info("La fecha es " + newYear);
+newYear.setMonth(-6); // Retrocedemos
